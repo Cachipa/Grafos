03a22021d043a5ea475722a7dc7f37a46cc16c47
Cachipa <63456620+Cachipa@users.noreply.github.com>
2022-06-01 16:57:13 -0300

    Add files via upload

--- Grafos/main.py
+++ Grafos/main.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from matplotlib import pyplot as plt
 
-with open(r"Estacoes.txt", 'r') as fp:   # Conta numero de linhas no arquivo
+with open(r"Estacoes.txt", 'r') as fp:  # Conta numero de linhas no arquivo
     n = len(fp.readlines())
 
 filename = "Estacoes.txt"
@@ -13,27 +13,52 @@ nome = list(range(0, n))
 x = list(range(0, n))
 y = list(range(0, n))
 
-
-for i in range(n):            # Le o arquivo com as estações e coordenadas, e armazena elas
+for i in range(n):  # Le o arquivo com as estações e coordenadas, e armazena elas
     file = fi.readline()
     file = file.split()
     nome[i] = file[0]
     x[i] = file[1]
     y[i] = file[2]
 
+x = [int(i) for i in x]
+y = [int(i) for i in y]
+
 nomes = {}
-etc = list(range(1, n+1))
+etc = list(range(1, n + 1))
 
 for j in range(n):
-    nomes[etc[j]] = nome[j]     #associa nome da estação com aresta
-
+    nomes[etc[j]] = nome[j]  # associa nome da estação com aresta
 
 for i in range(n):
-    g.add_node(etc[i], pos=(x[i], y[i]))   #cria aresta e suas cordenadas
+    g.add_node(etc[i], pos=(x[i], y[i]))  # cria aresta e suas cordenadas
 
+with open(r"Conexoes", 'r') as fp2:  # Conta numero de linhas no arquivo
+    p = len(fp2.readlines())
+
+filename = "Conexoes"
+fi2 = open(filename, "r")
+
+for k in range(p):
+    file = fi2.readline()
+    if file != "":
+        file = file.split()
+    else:
+        break
+
+    for l in range(int(file[2])):
+        file2 = fi2.readline()
+        edge = file2.split()
+        print(edge)
+        for i in range(n):
+            if (edge[0] == nome[i]):
+                temp = i
+
+        for j in range(n):
+            if (edge[1] == nome[j]):
+                temp2 = j
+
+        g.add_edge(etc[temp], etc[temp2])
 
 pos = nx.get_node_attributes(g, 'pos')
-nx.draw(g, pos,labels=nomes, with_labels=True)
+nx.draw(g, pos, labels=nomes, with_labels=True)
 plt.show()
-
-
