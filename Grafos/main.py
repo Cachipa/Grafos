a8b0e96aebae9ddc31ba67a011262c4a3bee49d1
Cachipa <63456620+Cachipa@users.noreply.github.com>
2022-06-06 10:34:06 -0300

    Add files via upload

--- Grafos/main.py
+++ Grafos/main.py
@@ -12,6 +12,7 @@ g = nx.Graph()
 nome = list(range(0, n))
 x = list(range(0, n))
 y = list(range(0, n))
+ndcor = list(range(0, n))
 
 for i in range(n):  # Le o arquivo com as estações e coordenadas, e armazena elas
     file = fi.readline()
@@ -19,6 +20,7 @@ for i in range(n):  # Le o arquivo com as estações e coordenadas, e armazena e
     nome[i] = file[0]
     x[i] = file[1]
     y[i] = file[2]
+    ndcor[i] = file[3]
 
 x = [int(i) for i in x]
 y = [int(i) for i in y]
@@ -30,18 +32,21 @@ for j in range(n):
     nomes[etc[j]] = nome[j]  # associa nome da estação com aresta
 
 for i in range(n):
-    g.add_node(etc[i], pos=(x[i], y[i]))  # cria aresta e suas cordenadas
+    g.add_node(etc[i], pos=(x[i], y[i]), nodecolor=ndcor[i])  # cria aresta e suas cordenadas
 
 with open(r"Conexoes", 'r') as fp2:  # Conta numero de linhas no arquivo
     p = len(fp2.readlines())
 
 filename = "Conexoes"
 fi2 = open(filename, "r")
+colors = {}
 
 for k in range(p):
     file = fi2.readline()
     if file != "":
         file = file.split()
+        cor = file[1]
+        print(cor)
     else:
         break
 
@@ -50,15 +55,19 @@ for k in range(p):
         edge = file2.split()
         print(edge)
         for i in range(n):
-            if (edge[0] == nome[i]):
+            if edge[0] == nome[i]:
                 temp = i
 
         for j in range(n):
-            if (edge[1] == nome[j]):
+            if edge[1] == nome[j]:
                 temp2 = j
 
-        g.add_edge(etc[temp], etc[temp2])
+        g.add_edge(etc[temp], etc[temp2], color =cor)
 
+
+edges = g.edges()
+colors = nx.get_edge_attributes(g, 'color').values()
+ndcolor = nx.get_node_attributes(g, 'nodecolor').values()
 pos = nx.get_node_attributes(g, 'pos')
-nx.draw(g, pos, labels=nomes, with_labels=True)
+nx.draw(g, pos, labels=nomes, with_labels=True, edge_color=colors, node_color=ndcolor)
 plt.show()
